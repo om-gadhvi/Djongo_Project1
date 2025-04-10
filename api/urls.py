80171e46e7e898974fee0546a46710331768a995
Shama-Cyphersol <shamadevops@gmail.com>
2025-04-10 17:16:28 +0530

    Implemented 'GET','POST','PUT','DELETE'

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('students/', views.studentViews),
+    path('students/', views.studentsView),
+    path('students/<int:pk>/', views.studentDetailView),
     
 ]
--- api/views.py
+++ api/views.py
@@ -17,12 +17,41 @@ from rest_framework.decorators import api_view
      
      
 #using Django Rest Framework to serialize data
-@api_view(['GET'])
-def studentViews(request):
+@api_view(['GET', 'POST'])
+def studentsView(request):
     if request.method == 'GET':
         #get all the data from student table
         student = Student.objects.all()
         serializer = StudentSerializer(student, many=True)
-        
-        return Response(serializer.data, status=status.HTTP_200_OK)         
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
+    
+ 
+@api_view(['GET', 'PUT', 'DELETE'])   
+def studentDetailView(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesNotExist:
+        return Response({'error': 'Student not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+    
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)  
     
